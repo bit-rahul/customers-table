59a57f1795cae2685ad182bf3955c1a71a7ccda3
Rahul <rahul.27bok@gmail.com>
2021-05-23 16:24:06 +0530

    pagination added

--- src/components/MTable.js
+++ src/components/MTable.js
@@ -10,7 +10,9 @@ import {
     Paper,
     Avatar,
     Grid,
-    Typography
+    Typography,
+    TablePagination,
+    TableFooter
 } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
@@ -48,7 +50,19 @@ const useStyles = makeStyles(theme => ({
 
 function MTable() {
     const classes = useStyles();
-    const [customerData, setCustomerData] = useState(null);
+    const [customerData, setCustomerData] = useState([]);
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
 
     useEffect(() => {
         fetch(
@@ -65,61 +79,74 @@ function MTable() {
     }, []);
 
     return (
-        <TableContainer component={Paper} className={classes.tableContainer}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell className={classes.tableHeadCell}>Customer name</TableCell>
-                        <TableCell className={classes.tableHeadCell}>Email</TableCell>
-                        <TableCell className={classes.tableHeadCell}>Phone</TableCell>
-                        <TableCell className={classes.tableHeadCell}>Premium</TableCell>
-                        <TableCell className={classes.tableHeadCell}>Max/Min bid</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {customerData && customerData.map((row, i) => (
-                        <TableRow key={i}>
-                            <TableCell>
-                                <Grid container>
-                                    <Grid item lg={2}>
-                                        <Avatar className={classes.avatarClass} alt={row.firstname + " " + row.lastname} src={row.avatarUrl} />
-                                    </Grid>
-                                    <Grid item lg={1}>&emsp;</Grid>
-                                    <Grid item lg={9}>
-                                        <Typography className={classes.nameStyle}>{row.firstname + " " + row.lastname}</Typography>
-                                    </Grid>
-                                </Grid>
-                            </TableCell>
-                            <TableCell>
-                                <Typography color="primary" variant="subtitle2">
-                                    {row.email}
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography color="textSecondary" variant="body2">
-                                    {row.phone}
-                                </Typography>
-                            </TableCell>
-                            <TableCell>
-                                <Typography className={classes.styleMember}
-                                    style={{
-                                        backgroundColor: row.hasPremium ? "green" : "orange"
-                                    }}
-                                >
-                                    {
-                                        row.hasPremium
-                                            ?
-                                            "True"
-                                            :
-                                            "False"
-                                    }
-                                </Typography></TableCell>
-                            <TableCell>{row.bids[0] ? row.bids[0].amount : null}</TableCell>
+        <>
+            <TableContainer component={Paper} className={classes.tableContainer}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell className={classes.tableHeadCell}>Customer name</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Email</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Phone</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Premium</TableCell>
+                            <TableCell className={classes.tableHeadCell}>Max/Min bid</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {customerData && customerData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, i) => (
+                            <TableRow key={i}>
+                                <TableCell>
+                                    <Grid container>
+                                        <Grid item lg={2}>
+                                            <Avatar className={classes.avatarClass} alt={row.firstname + " " + row.lastname} src={row.avatarUrl} />
+                                        </Grid>
+                                        <Grid item lg={1}>&emsp;</Grid>
+                                        <Grid item lg={9}>
+                                            <Typography className={classes.nameStyle}>{row.firstname + " " + row.lastname}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography color="primary" variant="subtitle2">
+                                        {row.email}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography color="textSecondary" variant="body2">
+                                        {row.phone}
+                                    </Typography>
+                                </TableCell>
+                                <TableCell>
+                                    <Typography className={classes.styleMember}
+                                        style={{
+                                            backgroundColor: row.hasPremium ? "green" : "orange"
+                                        }}
+                                    >
+                                        {
+                                            row.hasPremium
+                                                ?
+                                                "True"
+                                                :
+                                                "False"
+                                        }
+                                    </Typography></TableCell>
+                                <TableCell>{row.bids[0] ? row.bids[0].amount : null}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                    <TableFooter>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, 100]}
+                            component="div"
+                            count={customerData.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                        />
+                    </TableFooter>
+                </Table>
+            </TableContainer>
+        </>
     )
 }
 
