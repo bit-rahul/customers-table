99d10fd26d6213771a34ecd6c303b5951d2bcc7a
Rahul <rahul.27bok@gmail.com>
2021-05-23 16:14:44 +0530

    material-ui table with styles

--- src/components/MTable.js
+++ src/components/MTable.js
@@ -1,6 +1,53 @@
 import React, { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Avatar,
+    Grid,
+    Typography
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    table: {
+        // minWidth: 650,
+    },
+    tableContainer: {
+        borderRadius: 15,
+        margin: "50px auto",
+        maxWidth: "90%"
+    },
+    tableHeadCell: {
+        fontWeight: 'bold',
+        backgroundColor: theme.palette.primary.dark,
+        color: theme.palette.getContrastText(theme.palette.primary.dark)
+    },
+    avatarClass: {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.getContrastText(theme.palette.primary.light)
+    },
+    nameStyle: {
+        fontWeight: '600',
+        color: theme.palette.secondary.dark
+    },
+    styleMember: {
+        fontWeight: 'bold',
+        fontSize: '0.8rem',
+        color: 'white',
+        backgroundColor: 'grey',
+        padding: '2px 10px',
+        display: 'inline-block',
+        borderRadius: "6px"
+    }
+}));
 
 function MTable() {
+    const classes = useStyles();
     const [customerData, setCustomerData] = useState(null);
 
     useEffect(() => {
@@ -18,7 +65,61 @@ function MTable() {
     }, []);
 
     return (
-        <div/>
+        <TableContainer component={Paper} className={classes.tableContainer}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell className={classes.tableHeadCell}>Customer name</TableCell>
+                        <TableCell className={classes.tableHeadCell}>Email</TableCell>
+                        <TableCell className={classes.tableHeadCell}>Phone</TableCell>
+                        <TableCell className={classes.tableHeadCell}>Premium</TableCell>
+                        <TableCell className={classes.tableHeadCell}>Max/Min bid</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {customerData && customerData.map((row, i) => (
+                        <TableRow key={i}>
+                            <TableCell>
+                                <Grid container>
+                                    <Grid item lg={2}>
+                                        <Avatar className={classes.avatarClass} alt={row.firstname + " " + row.lastname} src={row.avatarUrl} />
+                                    </Grid>
+                                    <Grid item lg={1}>&emsp;</Grid>
+                                    <Grid item lg={9}>
+                                        <Typography className={classes.nameStyle}>{row.firstname + " " + row.lastname}</Typography>
+                                    </Grid>
+                                </Grid>
+                            </TableCell>
+                            <TableCell>
+                                <Typography color="primary" variant="subtitle2">
+                                    {row.email}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography color="textSecondary" variant="body2">
+                                    {row.phone}
+                                </Typography>
+                            </TableCell>
+                            <TableCell>
+                                <Typography className={classes.styleMember}
+                                    style={{
+                                        backgroundColor: row.hasPremium ? "green" : "orange"
+                                    }}
+                                >
+                                    {
+                                        row.hasPremium
+                                            ?
+                                            "True"
+                                            :
+                                            "False"
+                                    }
+                                </Typography></TableCell>
+                            <TableCell>{row.bids[0] ? row.bids[0].amount : null}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
